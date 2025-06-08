ee1efd00eb791dd06ab6fe4aec063563513270fe
ryxa <rayhansatriabudi@gmail.com>
2025-06-08 23:50:05 +0700

    menambah soal To-Lower-Case

---
+++ easy/hash-table/To-Lower-Case/709.to-lower-case.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=709 lang=python
+#
+# [709] To Lower Case
+#
+
+# @lc code=start
+class Solution(object):
+    def toLowerCase(self, s):
+        return s.lower()
+        
+# @lc code=end
+
