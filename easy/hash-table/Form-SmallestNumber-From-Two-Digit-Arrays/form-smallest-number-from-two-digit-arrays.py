f634757ef9c65e5265850c87dd5804b13422c089
ryxa <rayhansatriabudi@gmail.com>
2025-06-16 22:18:36 +0700

    menambah soal Form-SmallestNumber-From-Two-Digit-Arrays

---
+++ easy/hash-table/Form-SmallestNumber-From-Two-Digit-Arrays/form-smallest-number-from-two-digit-arrays.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=2605 lang=python3
+#
+#  Form Smallest Number From Two Digit Arrays
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
+        minimum = float("inf")
+        for n_1 in nums1:
+            for n_2 in nums2:
+                if n_1 == n_2:
+                    minimum = min(minimum, n_1)
+                else:
+                    minimum = min(minimum, int(f"{n_1}{n_2}"))
+                    minimum = min(minimum, int(f"{n_2}{n_1}"))
+        
+        return minimum
+    
+# Given two arrays of unique digits nums1 and nums2, return the smallest number that contains at least one digit from each array.
+
+# Example 1:
+
+# Input: nums1 = [4,1,3], nums2 = [5,7]
+# Output: 15
+# Explanation: The number 15 contains the digit 1 from nums1 and the digit 5 from nums2. It can be proven that 15 is the smallest number we can have.
+# Example 2:
+
+# Input: nums1 = [3,5,2,6], nums2 = [3,1,7]
+# Output: 3
+# Explanation: The number 3 contains the digit 3 which exists in both arrays.
+ 
+
+# Constraints:
+
+# 1 <= nums1.length, nums2.length <= 9
+# 1 <= nums1[i], nums2[i] <= 9
+# All digits in each array are unique.
+# @lc code=end
+
